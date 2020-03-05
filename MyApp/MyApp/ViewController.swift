d9eedf3912e777a9cf960fb2ff6c9030b8769ccf
曹世鑫 <cao_shixin@yahoo.com>
2020-03-05 15:18:16 +0800

    添加我的项目

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by 曹世鑫 on 2020/3/5.
+//  Copyright © 2020 曹世鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyApp/MyAppUITests/MyAppUITests.swift
@@ -0,0 +1,43 @@
+//
+//  MyAppUITests.swift
+//  MyAppUITests
+//
+//  Created by 曹世鑫 on 2020/3/5.
+//  Copyright © 2020 曹世鑫. All rights reserved.
+//
+
+import XCTest
+
+class MyAppUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
